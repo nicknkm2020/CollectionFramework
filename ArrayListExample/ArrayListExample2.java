1ec6ea720f791fe0bc5af865a39a3d99cb164b95
nmandl001 <namanmandli@gmail.com>
2017-03-15 22:40:03 -0400

    ArrayList

---
+++ ArrayListExample/ArrayListExample2.java
@@ -0,0 +1,20 @@
+package ArrayListExample;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample2
+{
+
+    public static void main(String[] args)
+    {
+        List<String> list = new ArrayList<String>();
+
+        list.add("Ram");
+        list.add("Dave");
+        list.add("Peter");
+        list.add("John");
+
+        System.out.println(list);
+    }
+
+}
