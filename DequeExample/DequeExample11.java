adc8a36a88524ddcb5a8eaf54b2f809d5c0712e5
nmandl001 <namanmandli@gmail.com>
2017-03-17 16:53:08 -0400

    DequeExample
    DequeExample

---
+++ DequeExample/DequeExample11.java
@@ -0,0 +1,29 @@
+package DequeExample;
+import java.util.Deque;
+import java.util.LinkedList;
+
+/*
+ *  Example of push(E e) method.
+ */
+public class DequeExample11
+{
+
+    public static void main(String[] args)
+    {
+        Deque<Integer> deque = new LinkedList<Integer>();
+        deque.add(2000);
+        deque.add(5000);
+        deque.add(10000);
+
+        System.out.println("deque : " + deque + "\n");
+
+        /*
+         * Pushes an element onto the stack represented by this deque (in other
+         * words, at the head of this deque)
+         */
+        deque.push(100);
+        System.out.println("deque : " + deque + "\n");
+
+    }
+
+}
