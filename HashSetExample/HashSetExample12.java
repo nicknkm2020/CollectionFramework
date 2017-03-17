c2bca5c677b8694fc0ce98189cde687df0d952c9
nmandl001 <namanmandli@gmail.com>
2017-03-17 17:53:17 -0400

    HashSetExample
    HashSetExample

---
+++ HashSetExample/HashSetExample12.java
@@ -0,0 +1,32 @@
+package HashSetExample;
+import java.util.HashSet;
+
+/*
+ * Example of remove(Object o) method.
+ */
+public class HashSetExample12
+{
+    public static void main( String[] args )
+    {
+        HashSet<String> hashSet = new HashSet<String>();
+
+        hashSet.add("Dave");
+        hashSet.add("Peter");
+        hashSet.add("Phil");
+        hashSet.add("Rohit");
+        hashSet.add("Virat");
+
+        System.out.println("hashSet : " + hashSet + "\n");
+
+        /*
+         * Removes the specified element from this set if it is present.
+         */
+
+        boolean isRemoved = hashSet.remove("Virat");
+        System.out.println("isRemoved : " + isRemoved);
+
+        System.out.println("hashSet : " + hashSet + "\n");
+
+    }
+    
+}
---
+++ HashSetExample/HashSetExample19.java
@@ -0,0 +1,30 @@
+package HashSetExample;
+import java.util.HashSet;
+
+/*
+ * Example of clone() method.
+ */
+public class HashSetExample19
+{
+    public static void main( String[] args )
+    {
+        HashSet<String> hashSet = new HashSet<String>();
+
+        hashSet.add("Dave");
+        hashSet.add("Peter");
+        hashSet.add("Phil");
+        hashSet.add("Rohit");
+        hashSet.add("Virat");
+
+        System.out.println("hashSet: " + hashSet + "\n");
+
+        /*
+         * Returns a shallow copy of this HashSet instance: the elements
+         * themselves are not cloned.
+         */
+        HashSet<String> clonedHashSet = (HashSet<String>) hashSet.clone();
+
+        System.out.println("clonedHashSet : " + clonedHashSet);
+
+    }
+}
