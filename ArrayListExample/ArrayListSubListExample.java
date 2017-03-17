f8b0e0f2b1bc8f7fb7701a82546bf1a1e9eacfee
nmandl001 <namanmandli@gmail.com>
2017-03-16 21:19:18 -0400

    LinkedLIst and ArrayList
    LinkedLIst and ArrayList

---
+++ ArrayListExample/ArrayListSubListExample.java
@@ -0,0 +1,28 @@
+package ArrayListExample;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Example of subList(int fromIndex, int toIndex) method
+ */
+public class ArrayListSubListExample
+{
+
+    public static void main( String[] args )
+    {
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("Ram");
+        arrayList.add("Dave");
+        arrayList.add("Peter");
+        arrayList.add("Julia");
+        arrayList.add("Akram");
+
+        System.out.println("arrayList : " + arrayList + "\n");
+
+        List<String> subList = arrayList.subList(0, 3);
+
+        System.out.println("subList : " + subList + "\n");
+
+    }
+
+}
---
+++ LinkedListExample/LinkedListExample2.java
@@ -0,0 +1,24 @@
+package LinkedListExample;
+import java.util.LinkedList;
+/*
+ * Example of add(E e) method.
+ */
+public class LinkedListExample2
+{
+
+    public static void main( String[] args )
+    {
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+
+        /*
+         * Appends the specified element to the end of this list.
+         */
+        linkedList.add(100);
+        linkedList.add(200);
+        linkedList.add(300);
+        linkedList.add(400);
+
+        System.out.println("linkedList : "+linkedList);
+    }
+
+}
