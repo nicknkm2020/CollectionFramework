df41596a6b69220e293f6351727ae47b6284b2e7
nmandl001 <namanmandli@gmail.com>
2017-03-19 00:54:59 -0400

    HashMapExample

---
+++ HashMapExample/HashMapExample2.java
@@ -0,0 +1,36 @@
+package HashMapExample;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+/*
+ * Example of HashMap(Map<? extends K,? extends V> m) constructor.
+ */
+public class HashMapExample2
+{
+    public static void main( String[] args )
+    {
+
+        TreeMap<Integer, String> treeMap = new TreeMap<Integer, String>();
+
+        treeMap.put(1, "Apple");
+        treeMap.put(2, "Ball");
+        treeMap.put(3, "Cat");
+        treeMap.put(4, "Cat");
+        treeMap.put(5, "Cat");
+        treeMap.put(6, "Cat");
+        treeMap.put(7, "Cat");
+
+        System.out.println("treeMap : " + treeMap+"\n");
+
+        /*
+         * Constructs a new HashMap with the same mappings as the specified Map.
+         * The HashMap is created with default load factor (0.75) and an initial
+         * capacity sufficient to hold the mappings in the specified Map.
+         */
+
+        HashMap<Integer, String> hashMap = new HashMap<Integer, String>(treeMap);
+
+        System.out.println("hashMap : " + hashMap);
+
+    }
+}
